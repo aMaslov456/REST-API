de4baf10c51c86dcf01f6f35bafcde39c5fedbd5
Artem Maslov <AMaslov@mtt.ru>
2022-02-13 11:03:02 +0300

    release v1

---
+++ cmd/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	todo "github.com/aMaslov456/REST-API"
+	handler "github.com/aMaslov456/REST-API/pkg/handler"
+	"github.com/aMaslov456/REST-API/pkg/repository"
+	"github.com/aMaslov456/REST-API/pkg/service"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+	"log"
+	"os"
+)
+
+func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
+	if err := initConfig(); err != nil {
+		logrus.Fatalf("error initializing configs:%s", err.Error())
+	}
+
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPosgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+	repos := repository.NewRepository(db)
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
+	srv := new(todo.Server)
+	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
+}
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
+}
---
+++ pkg/handler/handler.go
@@ -0,0 +1,49 @@
+package handler
+
+import (
+	"github.com/aMaslov456/REST-API/pkg/service"
+	"github.com/gin-gonic/gin"
+)
+
+type Handler struct {
+	services *service.Service
+}
+
+func NewHandler(services *service.Service) *Handler {
+	return &Handler{services: services}
+}
+
+func (h *Handler) InitRoutes() *gin.Engine {
+	router := gin.New()
+
+	auth := router.Group("/auth")
+	{
+		auth.POST("/sign-up")
+		auth.POST("/sign-in")
+	}
+	api := router.Group("/api")
+	{
+		lists := api.Group("/lists")
+		{
+			lists.POST("/", h.createList)
+			lists.GET("/", h.getAllList)
+			lists.GET("/:id", h.getListbyId)
+			lists.PUT("/:id", h.updateList)
+			lists.DELETE("/:id", h.deleteList)
+
+			items := lists.Group(":id/items")
+			{
+				items.POST("/", h.createItem)
+				items.GET("/", h.getAllItem)
+			}
+		}
+
+		items := api.Group("items")
+		{
+			items.GET("/:id", h.getItembyId)
+			items.PUT("/:id", h.updateItem)
+			items.DELETE("/:id", h.deleteItem)
+		}
+	}
+	return router
+}
